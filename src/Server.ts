07d190021aee041e4b5749768f4ee65b054f6728
andresfdel17 <andresfdel13@hotmail.com>
2022-10-18 21:02:55 -0500

    ajustes de rutas en ts

--- src/Server.ts
+++ src/Server.ts
@@ -1,11 +1,12 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response, Router } from 'express';
 import dotenv from 'dotenv';
+import Login from "./controllers/Login";
 
 dotenv.config();
 
 const app: Express = express();
 app.set("PORT", process.env.PORT || 3000);
-
+app.use("/login", Login);
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
