7ce89bd59d66c9fa0c571852a4f4c1cb5d05d8d2
andresfdel17 <andresfdel13@hotmail.com>
2022-11-04 14:30:29 -0500

    ajustes de backend, preparaciones para conectar a bd

--- src/Server.ts
+++ src/Server.ts
@@ -1,13 +1,13 @@
-import express, { Express, Request, Response, Router } from 'express';
+import express, { Express, Request, Response } from "express";
 import cors from "cors";
-import path from 'path';
-import dotenv from 'dotenv';
+import path from "path";
+import dotenv from "dotenv";
+
+
 //Controllers
 import Login from "./controllers/Login";
-import { PasswordManager } from './lib/PasswordManager';
 
 dotenv.config();
-
 const app: Express = express();
 const corsConfig = {
   origin: true,
@@ -19,15 +19,11 @@ app.set("PORT", process.env.PORT || 3000);
 app.use(express.urlencoded({ extended: false }));
 // parse requests of content-type - application/json
 app.use(express.json());
-app.use('/public', express.static(path.join(__dirname, '../public')));
+app.use("/public", express.static(path.join(__dirname, "../public")));
 //Rutas de controladores
 app.use("/login", Login);
-app.get('/',async (req: Request, res: Response) => {
-  let pass = await PasswordManager.hashPassword("test");
-  console.log(pass);
-  
-  res.send('Express + TypeScript Server');
+app.get("/", async (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
 });
-
 
 export default app;
--- src/controllers/Login.ts
+++ src/controllers/Login.ts
@@ -1,9 +1,13 @@
 import { Request, Response, Router } from "express";
+import { ILoginRequest } from "../interfaces/controllers/Login/ILogin";
 const router: Router = Router();
 
 router.get(`/`, (req: Request, res: Response) => {
-    res.send("Login")
+  res.send("Login");
 });
 
+router.post("/login", async (req: Request<ILoginRequest>, res: Response) => {
+    console.log(req.body);
+});
 
 export default router;
